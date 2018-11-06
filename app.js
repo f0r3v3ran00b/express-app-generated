b98bb9af1097a03fb78a0108792fb534afc0a229
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-06 12:36:10 +1100

    will this work

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 var logger = require('morgan');
 const serverless = require('serverless-http');
 
@@ -23,12 +24,16 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
+app.use(bodyParser);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/vices', vicesRouter);
+
+// For Netlify Lambda
+app.use('/.netlify/functions/vices', vicesRouter); // path must route to lambda
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
